0108059ce09cda1e9735d2091a6bc18b8e1adca2
igor.korolyev <korolew.fw@yandex.ru>
2022-10-16 12:08:17 +0300

    new

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,6 +1,7 @@
 from pom.index_page import Authorization
 import pytest
 import random
+import allure
 
 random_nambers = [1,2,3]
 
@@ -8,16 +9,23 @@ random_nambers = [1,2,3]
 def test_1():
     assert 1 == random.choice(random_nambers)
 
+@allure.story("тесты про авторизацию")
 @pytest.mark.usefixtures('setup')
 class TestAuthorization:
 
+    @allure.label("owner","Igor")
+    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.description("проверка входа в систему")
     @pytest.mark.smoke
     def test_successful_login(self):
         driver = self.driver
         step = Authorization(driver)
-        step.enter_correct_username()
-        step.enter_correct_password()
-        step.clik_login_button()
+        with allure.step("шаг1"):
+            step.enter_correct_username()
+        with allure.step("шаг2"):
+            step.enter_correct_password()
+        with allure.step("шаг3"):
+            step.clik_login_button()
 
 
 
