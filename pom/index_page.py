8a77874d26a26238dffddb493ac23f08ba533b6f
igor.korolyev <korolew.fw@yandex.ru>
2022-10-22 13:24:14 +0300

    test1

--- pom/index_page.py
+++ pom/index_page.py
@@ -1,6 +1,13 @@
 from base.base_object import BaseObject
 from selenium.common.exceptions import NoSuchElementException
 from locators.locators_list import IndexPageLocators as ind
+import json
+
+
+file = open('passwords.json')
+username_pass = json.load(file)
+username = username_pass['username']
+password = username_pass['password']
 
 
 class Authorization(BaseObject):
@@ -10,10 +17,10 @@ class Authorization(BaseObject):
         self.driver = driver
 
     def enter_correct_username(self):
-        self.is_visible('id', ind.FIND_USERNAME_FIELD_ID).send_keys('standard_user')
+        self.is_visible('id', ind.FIND_USERNAME_FIELD_ID).send_keys(username)
 
     def enter_correct_password(self):
-        self.is_visible('id', ind.FIND_PASSWORD_FIELD_ID).send_keys('secret_sauce')
+        self.is_visible('id', ind.FIND_PASSWORD_FIELD_ID).send_keys(password)
 
     def clik_login_button(self):
         self.is_visible('id', ind.FIND_LOGIN_BUTTON_ID).click()
